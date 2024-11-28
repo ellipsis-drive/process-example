6b4a90324982a8f97ab519789d7130f791a56343
danielvandermaas <danielvandermaas@protonmail.com>
2024-11-28 17:21:47 +0100

    x

--- test.py
+++ test.py
@@ -4,13 +4,6 @@ import numpy as np
 
 import sys
 
-<<<<<<< HEAD
-#el.apiManager.baseUrl = 'https://api.esa.ellipsis-drive.com/v3'
-
-=======
-el.apiManager.baseUrl = 'https://api.esa.ellipsis-drive.com/v3'
-ai.url = 'https://api.esa.ellipsis-drive.com/v3'
->>>>>>> 7c2be7897787b53c0d3fdfb6345a549515d82de1
 
 pathId = 'a479869c-caa6-4b95-acb7-0ce1cfe71828'
 timestampId = "Not available, activate first"
@@ -19,21 +12,13 @@ targetPathId = "19b30a1c-2ea3-49ca-a859-2ff3bd731abe"
 
 
 pathId = sys.argv[1]
-<<<<<<< HEAD
-timestampId =  int(sys.argv[2])
-targetPathId = int(sys.argv[3])
-
-=======
 timestampId =  sys.argv[2]
 targetPathId = sys.argv[3]
 try:
     token = sys.argvd[4]
 except:
     token = None
->>>>>>> 7c2be7897787b53c0d3fdfb6345a549515d82de1
 
-
-token = el.auth()
 
 #retrieve the zoom and bounds of the capture you wish to classify
 classificationZoom = ai.getReccomendedClassificationZoom(pathId = pathId, timestampId = timestampId, token = token)
